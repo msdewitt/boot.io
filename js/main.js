e99e5ae7a8fd0254f16759ffc4c109ba7dc06b11
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-07 13:08:35 -0400

    Add libraries and a test sprite

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-var Q = Quintus().include('Sprites, Scenes, Input').setup("bootIOGame");
+var Q = Quintus().include("Sprites").setup();
+
+Q.Sprite.extend("Ball",{
+  init:function(p) {
+    this._super(p,{
+      x: 0,
+      y: 300,
+      vx: 25,
+      vy: -100,
+      w: 100,
+      h: 100,
+      asset: "ball.png",
+    });
+  },
+
+  step: function(dt) {
+    this.p.vy += dt * 9.8;
+
+    this.p.x += this.p.vx * dt;
+    this.p.y += this.p.vy * dt;
+  }
+});
+
+Q.load(["ball.png"],function() {
+  var ball = new Q.Ball();
+  Q.gameLoop(function(dt) {
+    ball.update(dt);
+    Q.clear();
+    ball.render(Q.ctx);
+  });
+});
